945e2a4550f288ff43790334830e22816e5fe332
Hariom Singh <hk1587403@gmail.com>
2025-07-14 06:33:55 +0000

    Remove all the animation and logo part and use the code which was using

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,15 +1,4 @@
-import Image from 'next/image';
 
-const Logo = () => (
-  <div className="flex items-center justify-center h-screen">
-    <Image
-      src="https://i.ibb.co/Vt65r2n/344873839-1234390740847039-4467597143953335759-n-removebg-preview.png"
-      alt="Logo"
-      width={500}
-      height={500}
-      className="animate-in fade-in"
-    />
-  </div>
-);
+const Logo = () => null;
 
 export default Logo;
