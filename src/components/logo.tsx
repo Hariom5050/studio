b3fd59d730060a20b9f7a595972a73a167975774
Hariom Singh <hk1587403@gmail.com>
2025-07-14 07:41:31 +0000

    Now also it shows image not found

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,14 +1,28 @@
-import Image from 'next/image';
+import React from 'react';
 
 const Logo = () => {
   return (
-    <img
-      src="https://i.ibb.co/b3C3N9b/kws-logo-white-bg.jpg"
-      alt="Kingswood World School Logo"
-      width={40}
-      height={40}
-      className="rounded-full"
-    />
+    <svg
+      width="40"
+      height="40"
+      viewBox="0 0 100 100"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+    >
+      <defs>
+        <clipPath id="circleClip">
+          <circle cx="50" cy="50" r="50" />
+        </clipPath>
+      </defs>
+      <image
+        xlinkHref="https://i.ibb.co/b3C3N9b/kws-logo-white-bg.jpg"
+        x="0"
+        y="0"
+        width="100"
+        height="100"
+        clipPath="url(#circleClip)"
+      />
+    </svg>
   );
 };
 
