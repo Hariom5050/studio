16fac3267264de3a06e92fe7c5f6997c3a06d95c
Hariom Singh <hk1587403@gmail.com>
2025-07-15 05:51:33 +0000

    Make logo center align and increase a little size of the logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -6,8 +6,8 @@ const Logo = () => {
     <Image
       src="https://res.cloudinary.com/dtjjgiitl/image/upload/q_auto:good,f_auto,fl_progressive/v1752558405/scyjghobvnawzyuhfzho.jpg"
       alt="Kingswood World School Logo"
-      width={40}
-      height={40}
+      width={50}
+      height={50}
       className="rounded-full"
     />
   );
